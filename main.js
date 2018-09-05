e0e280dc8648227b4ca7d655c4986141443f57c8
Nato Boram <nato.boram@gmail.com>
2018-09-05 16:16:57 -0400

    Stuff is in the html file!

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use scrict";
 const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -9,10 +10,7 @@ function createWindow() {
 	win = new BrowserWindow({ width: 800, height: 600 })
 
 	// and load the index.html of the app.
-	win.loadFile('index.html')
-
-	// Open the DevTools.
-	win.webContents.openDevTools()
+	win.loadFile('html/index.html')
 
 	// Emitted when the window is closed.
 	win.on('closed', () => {
